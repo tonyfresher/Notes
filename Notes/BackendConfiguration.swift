0235832c5378eaca6d96af7441bc1b413a183547
Anton Frehser <farnetsohn@gmail.com>
2017-07-26 11:45:38 +0500

    some constatnts

--- Notes/BackendConfiguration.swift
+++ Notes/BackendConfiguration.swift
@@ -10,6 +10,58 @@ import Foundation
 
 class BackendConfiguration {
     
+    static let host = "notes.mrdekk.ru"
     
+    static let authHeader = "Autorization"
+    static let contentTypeHeader = "Content-Type"
     
+    // PART: OAuthRequest
+    
+    class OAuthRequest {
+        
+        static let scheme = "https"
+        
+        static let host = "oauth.yandex.ru"
+        
+        static let path = "/authorize"
+        
+        static let responseTypeName = "responseType"
+        static let responseType = "token"
+        
+        static let clientIdName = "client_id"
+        static let clientId = "b810a64aa3454975bb665186bc4350cd"
+
+        static var request = { () -> URL in
+            var components = URLComponents()
+            components.scheme = scheme
+            components.host = host
+            components.path = path
+            components.queryItems = [
+                URLQueryItem(name: responseTypeName, value: responseType),
+                URLQueryItem(name: clientIdName, value: clientId)
+            ]
+            
+            return components.url!
+        }()
+        
+    }
+    
+    // PART: OAuthResponse
+    
+    class OAuthResponse {
+        
+        enum Arguments: String {
+            
+            case accessToken = "access_token"
+            
+            case expiresIn = "expires_in"
+            
+            case tokenType = "token_type"
+            
+            case state = "state"
+            
+        }
+        
+    }
+
 }
--- Notes/LoginViewController.swift
+++ Notes/LoginViewController.swift
@@ -9,26 +9,49 @@
 import UIKit
 import WebKit
 
-class LoginViewController: UIViewController, WKUIDelegate {
+class LoginViewController: UIViewController, WKUIDelegate, WKNavigationDelegate {
     
     var webView: WKWebView!
     
-    override func loadView() {
-        let webConfiguration = WKWebViewConfiguration()
-        
-        webView = WKWebView(frame: .zero, configuration: webConfiguration)
-        
-        webView.uiDelegate = self
-        
-        view = webView
-    }
-    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        let myURL = URL(string: "https://www.apple.com")!
-        let request = URLRequest(url: myURL)
+        
+        webView = WKWebView()
+        webView.uiDelegate = self
+        webView.navigationDelegate = self
+        
+        view = webView
+        
+        //        view.addSubview(webView)
+        //
+        //        topLayoutGuide.bottomAnchor.constraint(equalTo: webView.topAnchor).isActive = true
+        //        bottomLayoutGuide.topAnchor.constraint(equalTo: webView.bottomAnchor).isActive = true
+        //        view.leadingAnchor.constraint(equalTo: webView.leadingAnchor).isActive = true
+        //        view.trailingAnchor.constraint(equalTo: webView.trailingAnchor).isActive = true
+        
+        let request = URLRequest(url: BackendConfiguration.OAuthRequest.request)
         webView.load(request)
     }
-
+    
+    func webView(_ webView: WKWebView, decidePolicyFor navigationAction: WKNavigationAction, decisionHandler: @escaping (WKNavigationActionPolicy) -> Void) {
+        guard let url = navigationAction.request.url,
+            let urlComponents = URLComponents(url: url, resolvingAgainstBaseURL: false),
+            let fragment = urlComponents.fragment,
+            urlComponents.host == BackendConfiguration.host else {
+                decisionHandler(.allow)
+                return
+        }
+        
+        let arguments = fragment
+            .components(separatedBy: "&")
+            .map { $0.components(separatedBy: "=") }
+        guard let accessTokenArgument = arguments.first(where: { $0[0] == BackendConfiguration.OAuthResponse.Arguments.accessToken.rawValue }) else {
+            decisionHandler(.allow)
+            return
+        }
+        
+        let accessToken = accessTokenArgument[1]
+        // TODO: SAVE AND SEGUE
+        decisionHandler(.cancel)
+    }
 }
